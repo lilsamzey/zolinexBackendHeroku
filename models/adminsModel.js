71eff1c269d5733c9646b1985af687adaa716135
ALI OZKARA <113827860+lilsamzey@users.noreply.github.com>
2023-12-18 19:58:58 +0100

    Update adminsModel.js

--- models/adminsModel.js
+++ models/adminsModel.js
@@ -55,6 +55,37 @@ exports.getAllAdmins = async () => {
 
 
 
+
+
+  exports.updateAdmin = async (id, admin) => {
+    try {
+      const pool = await sql.connect(config);
+  
+      const result = await pool.request().query(`
+        EXEC updateAdmin
+          '${id}',
+          '${admin.firstName}',
+          '${admin.lastName}',
+          '${admin.email}',
+          '${admin.address}',
+          '${admin.mobile}'
+      `);
+  
+      return result.recordset;
+    } catch (error) {
+      throw new Error('admin didnt updated');
+    } 
+  };
+
+
+
+
+
+
+
+
+
+
   exports.deleteAdmin = async (id) => {
     try {
       const pool = await new sql.connect(config);
@@ -118,6 +149,24 @@ exports.getAllAdminSettingsInfo = async (userId) => {
       return result.recordset;
     } catch (error) {
       throw new Error('An error occurred while updateing students password');
+     } 
+    
+  };
+
+
+
+
+
+  exports.getAdminDetailsByAdminId = async (adminId) => {
+ 
+    try {
+  
+     
+     const pool = await new sql.connect(config);
+      const result = await pool.request().query(`EXEC getAdminDetailsByAdminId @adminId =${adminId}`);
+      return result.recordset;
+    } catch (error) {
+      throw new Error('An error occurred while fetching admindetails');
      } 
     
   };
