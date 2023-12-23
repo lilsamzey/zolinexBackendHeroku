9a852dd73d6a692306ca3ef812a54d0c414f5312
ALI OZKARA <113827860+lilsamzey@users.noreply.github.com>
2023-12-23 20:43:05 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
- // Yeni ekledik
+const https = require('https');
+const fs = require('fs');
 
-
+// Yollar (routes) için gereken diğer modüller...
 const activityLogsRoutes = require('./routes/activityLogsRoutes');
 const studentsRoutes = require('./routes/studentsRoutes');
 const teachersRoutes = require('./routes/teachersRoutes');
@@ -14,20 +15,22 @@ const chatRoutes = require('./routes/chatRoutes');
 const filesRoutes = require('./routes/filesRoutes');
 const emailRoutes = require('./routes/emailRoutes');
 
-
-
-
 const app = express();
+
+// SSL/TLS sertifika dosyalarının yolu
+const sslOptions = {
+  key: fs.readFileSync('/etc/letsencrypt/live/subzolinex.zolinex.de/privkey.pem'),
+  cert: fs.readFileSync('/etc/letsencrypt/live/subzolinex.zolinex.de/fullchain.pem')
+};
+
+// Uygulamanın kullanacağı port, environment variable ile belirlenebilir ya da default 3000
 const PORT = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
 app.use(cors());
 
-
-
-
+// Yollarınızın (routes) tanımlamaları...
 app.use('/activitylogs', activityLogsRoutes);
-
 app.use('/students', studentsRoutes);
 app.use('/teachers', teachersRoutes);
 app.use('/courses', coursesRoutes);
@@ -35,10 +38,9 @@ app.use('/users', usersRoutes);
 app.use('/admins', adminsRoutes);
 app.use('/chat', chatRoutes);
 app.use('/files', filesRoutes);
+app.use('/email', emailRoutes); // Yeni ekledik
 
-// Yeni ekledik
-app.use('/email', emailRoutes);
-
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+// HTTPS sunucusunu başlat
+https.createServer(sslOptions, app).listen(PORT, () => {
+  console.log(`HTTPS Server is running on port ${PORT}`);
 });
